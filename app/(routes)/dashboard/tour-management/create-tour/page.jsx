6ca477e9c7317758424d1128c206a385f1e3e891
Sabbir Hossain <sabbir2always@gmail.com>
2024-11-15 00:54:59 +0600

    add new one

--- app/(routes)/dashboard/tour-management/create-tour/page.jsx
+++ app/(routes)/dashboard/tour-management/create-tour/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { MapContextProvider } from "./MapContext";
-import CreateTour from "./CreateTour";
+import { MapContextProvider } from "@/app/components/dashboard/newTour/MapContext";
+import CreateTour from "@/app/components/dashboard/newTour/CreateTour";
 export default function page({ pageType }) {
   return (
     <>
